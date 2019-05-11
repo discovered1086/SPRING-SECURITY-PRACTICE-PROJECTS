d387127242c35fb5369400439b92b447b72edaa5
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-05-11 17:10:19 -0500

    Adding all changes

--- SecuringMySpringMvcApp/src/main/java/com/kingshuk/springsecurityprojects/securingmvc/controller/UserController.java
+++ SecuringMySpringMvcApp/src/main/java/com/kingshuk/springsecurityprojects/securingmvc/controller/UserController.java
@@ -8,10 +8,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,8 +21,8 @@ import com.kingshuk.springsecurityprojects.securingmvc.persistence.UserRepositor
 @Controller
 @RequestMapping("/")
 public class UserController {
-
-    private final UserRepository userRepository;
+	
+	private final UserRepository userRepository;
 
     //
 
